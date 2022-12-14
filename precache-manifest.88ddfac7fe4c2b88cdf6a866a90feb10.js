f0bb6060471d12f437b13ed0ff74721ee3eeba66
KBWan <khanwan2@gmail.com>
2022-12-13 23:32:06 -0600

    fixed build

---
+++ precache-manifest.88ddfac7fe4c2b88cdf6a866a90feb10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f428cb4ec8b48b115416",
+    "url": "./static/css/main.a9bd4a22.chunk.css"
+  },
+  {
+    "revision": "f428cb4ec8b48b115416",
+    "url": "./static/js/main.f428cb4e.chunk.js"
+  },
+  {
+    "revision": "bc20d6d6d939309784cb",
+    "url": "./static/js/1.bc20d6d6.chunk.js"
+  },
+  {
+    "revision": "85b8a068c639b6533124",
+    "url": "./static/js/2.85b8a068.chunk.js"
+  },
+  {
+    "revision": "a97b32da44ab8b6db0a2",
+    "url": "./static/js/runtime~main.a97b32da.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d4992387e1e51e5676fafed87b89c04a",
+    "url": "./index.html"
+  }
+];
