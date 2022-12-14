b6e447bccf8227156be6f26340f1bca9cb3364bb
KBWan <khanwan2@gmail.com>
2022-12-13 23:15:26 -0600

    first commit

---
+++ precache-manifest.68950c0e7dc6a1f43d4c4986d3816b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f428cb4ec8b48b115416",
+    "url": "/static/css/main.a9bd4a22.chunk.css"
+  },
+  {
+    "revision": "f428cb4ec8b48b115416",
+    "url": "/static/js/main.f428cb4e.chunk.js"
+  },
+  {
+    "revision": "bc20d6d6d939309784cb",
+    "url": "/static/js/1.bc20d6d6.chunk.js"
+  },
+  {
+    "revision": "85b8a068c639b6533124",
+    "url": "/static/js/2.85b8a068.chunk.js"
+  },
+  {
+    "revision": "7889c2fc4b870185077c",
+    "url": "/static/js/runtime~main.7889c2fc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c37362fa191b5f0a4f845209cc26b852",
+    "url": "/index.html"
+  }
+];
